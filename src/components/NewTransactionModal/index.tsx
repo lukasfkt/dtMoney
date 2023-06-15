3179577033b1df574915110a3510b79c8b07c1df
Tutty <tuttytanaka@hotmail.com>
2023-06-14 23:11:12 -0300

    Creatting context, formmat data and currency

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Content, Overlay } from "./styles";
+import {
+  Content,
+  Overlay,
+  CloseButton,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
   return (
@@ -8,14 +15,27 @@ export function NewTransactionModal() {
 
       <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
+        <CloseButton>
+          <X />
+        </CloseButton>
         <form action="">
           <input type="text" placeholder="Descrição" required />
           <input type="text" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
 
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type="submit">Cadastrar</button>
         </form>
-        <Dialog.Close />
       </Content>
     </Dialog.Portal>
   );
